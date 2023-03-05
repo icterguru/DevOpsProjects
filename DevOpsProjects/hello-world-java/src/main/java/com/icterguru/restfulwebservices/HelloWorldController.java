2625dd7c6e16ca2d6619d775d86ed8468e5e3b10
ICTER Guru <ictergiru@gmail.com>
2023-03-04 20:01:55 -0500

    Merged

---
+++ DevOpsProjects/hello-world-java/src/main/java/com/icterguru/restfulwebservices/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.icterguru.restfulwebservices;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	@GetMapping(path = "/")
+	public String helloWorld() {
+		return "{\"message\":\"Hello World Java ICTER Guru Docker v2\"}";
+	}
+}
